8b52fdb4f2f886039f60e160846e5186b0cc8ae8
BartoszSzulcTamago <bartosz.szulc@tamago.software>
2024-03-04 11:08:31 +0100

    404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero__template  bg-color7 relative py-half lg:py-full overflow-hidden wysiwyg">
-  <div class="pointer-events-none bg-white absolute triangle-right -top-px -right-px h-20 w-20 lg:w-[160px] lg:h-[160px] z-10"></div>
+<section class="hero__template  bg-color4 relative py-half lg:py-full overflow-hidden wysiwyg">
   <div class="container">
       <div class="w-full mb-half-mobile lg:mb-half">
           <div class="text-h4 lg:text-h3 font-bold"  data-aos="fade-up">
