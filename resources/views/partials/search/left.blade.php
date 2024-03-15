5672d5d755acf3c12475fa4a44358c08bfd815ab
BartoszSzulcTamago <bartosz.szulc@tamago.software>
2024-03-15 12:28:34 +0100

    images change

--- resources/views/partials/search/left.blade.php
+++ resources/views/partials/search/left.blade.php
@@ -8,6 +8,8 @@
     <input autocomplete="off" name="hidden" type="text" class="absolute opacity-0 w-full h-full flex cursor-pointer" id="minMaxExample" aria-label="date">
     <div class="input-wrapper flex items-center gap-2.5 px-2.5 xs:px-5 3xl:px-30">
         @svg('images.kalendarz-big', 'max-3xl:max-w-[25px] max-3xl:h-6') 
+
+       
         <span id="selectedDate" class="flex items-center gap-2.5">{{ pll__('Wybierz datę') }}</span>
     </div>
 </div>
